f8f6c1b67b73c4ac7325577d06884e24c8f4184c
un-know-n <fa1thlezss@gmail.com>
2023-01-24 11:21:29 +0200

    Tasks finished

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+    String quote = "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.";
+    String author = "Ralph Waldo Emerson";
+    
+    public void display() {
+        System.out.println(quote + "\n - " + author);
+    }
+}
